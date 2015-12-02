faef1ed5b392fdf9170e815115eb50e774572a39
vagrant <vagrant@server-perf.vbx>
2015-12-01 21:32:12 -0500

    fixed jetty start command and nodejs Dockerfile COPY statement

--- jetty/docker_run.sh
+++ jetty/docker_run.sh
@@ -3,20 +3,19 @@
 img_name='serverperf/jetty'
 container_name='server_perf_jetty'
 shell_cmd='/bin/bash'
-start_cmd='java \
-    -Dcom.sun.management.jmxremote \
-    -Dcom.sun.management.jmxremote.ssl=false \
-    -Dcom.sun.management.jmxremote.authenticate=false \
-    -Dcom.sun.management.jmxremote.local.only=false \    
-    -Djava.rmi.server.hostname=localhost \
-    -Dcom.sun.management.jmxremote.port=1099 \
--jar /jetty/default/start.jar OPTIONS=Server,jmx etc/jetty-jmx.xml etc/jetty.xml'
+start_cmd="java \
+-Dcom.sun.management.jmxremote \
+-Dcom.sun.management.jmxremote.ssl=false \
+-Dcom.sun.management.jmxremote.authenticate=false \
+-Dcom.sun.management.jmxremote.local.only=false \
+-Dcom.sun.management.jmxremote.port=1099 \
+-jar /jetty/default/start.jar OPTIONS=Server,jmx etc/jetty-jmx.xml etc/jetty.xml"
 shared_volume_1="$PWD/base:/base"
 port_map_1='0.0.0.0:5050:8080'
-port_map_2='0.0.0.0:10001:1099'
+port_map_2='0.0.0.0:20001:1099'
 daemon='-d'
 transient='--rm'
-mode=$transient
+mode=$daemon
 
 docker stop $container_name && docker rm $container_name
 
@@ -26,10 +25,10 @@ docker_cmd="docker run $mode -ti \
   -p $port_map_1 -p $port_map_2 \
   -h $container_name.dkr \
   $img_name \
-  $shell_cmd"
+  $start_cmd"
 
 echo "running command... $docker_cmd"
-#eval $docker_cmd
+eval $docker_cmd
 
 sleep 3s
 
