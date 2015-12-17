81fa9b9c540de472c5593d435a5e2eef6507b60c
Peter Doyle <peter@engine2.cleverfishsoftware.com>
2015-12-17 10:18:35 -0700

    added additional monitoring scripts for nodejs containers

---
+++ tomcat/warmup_servers.sh
@@ -0,0 +1,8 @@
+
+
+
+for i in {1..50}; do curl -i -X GET "http://localhost:5040/servlet/perf/async?sleep=500" & done; wait
+for i in {1..50}; do curl -i -X GET "http://localhost:5040/servlet/perf?sleep=500" & done; wait
+
+for i in {1..50}; do curl -i -X GET "http://localhost:5040/servlet/perf/async?size=500" & done; wait
+for i in {1..50}; do curl -i -X GET "http://localhost:5040/servlet/perf?size=500" & done; wait
