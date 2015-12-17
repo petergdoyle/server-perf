99fba13653b39da5667143742608f82c6f0e367d
petergdoyle <peter.g.doyle@gmail.com>
2015-12-16 20:44:00 -0700

    found bug in supervisor.conf

---
+++ scripts/check_network_socket_state.sh
@@ -0,0 +1,17 @@
+
+ip=$1
+if [ -n "$ip" ]; then
+  echo "found"
+else
+  while [[ -z "$ip" ]]
+  do
+    echo -n "Enter remote ip to check connection state "
+    read ip
+  done
+fi
+
+cmd="netstat -anl | grep $ip | awk '/^tcp/ {t[$NF]++}END{for(state in t){print state, t[state]} }'"
+
+echo "running $cmd..."
+
+eval $cmd
