76c91d3cb5189234c0ebf412951df62f2b8b25d2
petergdoyle <peter.g.doyle@gmail.com>
2015-12-26 22:09:01 -0500

    added a server to test nodejs without express

--- nodejs/http_server.js
+++ nodejs/http_server.js
@@ -2,11 +2,12 @@ var http = require('http');
 var COUNTER = 0;
 var host = '0.0.0.0';
 var port = 5020;
+var context='/';
 http.createServer(function (req, res) {
   COUNTER++;
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.write('nodejs: ' + process.argv[1] + '\n');
-  res.write('Path: ' + context+ '\n');
+  res.write('Path: ' + context + '\n');
   res.write('Requests Processed:' + COUNTER+ '\n');
   res.write('Msg: server is running \n');
   res.end();
