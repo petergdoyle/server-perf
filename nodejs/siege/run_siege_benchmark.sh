1a06d952b19a37a1b4f7a61a1fe346ab586dbec2
petergdoyle <peter.g.doyle@gmail.com>
2015-12-26 21:25:26 -0500

    fixed siege script in nodejs/siege

--- nodejs/siege/run_siege_benchmark.sh
+++ nodejs/siege/run_siege_benchmark.sh
@@ -1,15 +1,20 @@
 
 
-siege_time=60
+siege_time='60'
 timeout_time=`expr $siege_time \* 3`
 shell_sleep_time='5m'
+host='engine2'
+port='5020'
+uri='/nodejs/perf'
+repetitions='5'
+
 rm ~/siege.log
 rm siege.log
-for i in {1..5}; do
+for i in $(eval echo "{1..$repetitions"}); do
     timestamp=$(date +%Y-%m-%d:%H:%M:%S)
-    echo "running siege $i at $timestamp..."
-    timeout $timeout_time's' siege -b 't'$siege_time's' http://engine2:5020/nodejs/perf
+    echo "running siege $i of $repetitions at $timestamp..."
+    timeout $timeout_time's' siege -b 't'$siege_time's' http://$host:$port$uri
     echo "done. sleeping $shell_sleep_time..."
     sleep $shell_sleep_time
 done
-cp ~/siege.log . 
+cp ~/siege.log ./siege.csv
