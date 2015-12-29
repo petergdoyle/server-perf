bdaaa89655f7932651aa9d5a6992a6607905c643
petergdoyle <peter.g.doyle@gmail.com>
2015-12-29 13:41:44 -0700

    work to date on scripts and servers and siege benchmarks

--- tomcat/siege/sync_sleep_0_size_0/run_siege_benchmark.sh
+++ tomcat/siege/sync_sleep_0_size_0/run_siege_benchmark.sh
@@ -1,1 +1,23 @@
-timeout siege -b t60s http://engine2:5040/servlet/perf > siege_benchmark.out & 
+
+siege_time='60'
+timeout_time=`expr $siege_time \* 3`
+shell_sleep_time='5m'
+host='engine2'
+port='5040'
+uri='/servlet/perf'
+repetitions='5'
+
+rm ~/siege.log
+rm siege.log
+for i in $(eval echo "{1..$repetitions"}); do
+    timestamp=$(date +%Y-%m-%d:%H:%M:%S)
+    echo "running siege $i of $repetitions at $timestamp..."
+    timeout $timeout_time's' siege -b 't'$siege_time's' http://$host:$port$uri
+    if [ "$i" -lt "$repetitions" ]; then
+        echo "done. sleeping $shell_sleep_time..."
+        sleep $shell_sleep_time
+    fi
+done
+cp ~/siege.log ./siege.csv
+
+
