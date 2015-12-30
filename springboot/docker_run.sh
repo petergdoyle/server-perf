8cb914beffb6c8ea6f1a3389598a742eb79790ba
Peter Doyle <peter@engine2.cleverfishsoftware.com>
2015-12-30 08:36:44 -0700

    mods to springboot to get Dockerized

--- springboot/docker_run.sh
+++ springboot/docker_run.sh
@@ -3,11 +3,8 @@
 img_name='serverperf/springboot'
 container_name='server_perf_springboot'
 shell_cmd='/bin/bash'
-start_cmd='/tomcat/default/bin/catalina.sh run'
-shared_volume_1="$PWD/webapps:/tomcat/default/webapps"
-port_map_1='0.0.0.0:5040:8080'
-port_map_2='0.0.0.0:10001:10001' #JMX rmi port client
-port_map_3='0.0.0.0:10002:10002' #JMX rmi port server
+docker_cmd='java -jar target/gs-actuator-service-0.1.0.jar'
+port_map_1='0.0.0.0:5070:5070'
 daemon='-d'
 transient='--rm'
 mode=$daemon
@@ -17,8 +14,7 @@ docker rm $container_name
 
 docker_cmd="docker run $mode -ti \
   --name $container_name \
-  -v $shared_volume_1 \
-  -p $port_map_1 -p $port_map_2 -p $port_map_3 \
+  -p $port_map_1 \
   -h $container_name.dkr \
   $img_name \
   $start_cmd"
