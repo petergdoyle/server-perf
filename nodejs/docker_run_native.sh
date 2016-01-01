f8b92cac2985c97259ea9a1062694095e52fbbd9
petergdoyle <peter.g.doyle@gmail.com>
2016-01-01 09:19:45 -0700

    testing new reusable docker scripts docker_run.sh

--- nodejs/docker_run_native.sh
+++ nodejs/docker_run_native.sh
@@ -1,39 +1,31 @@
 #!/bin/sh
 
+. ../scripts/lib/docker_run.sh
+
 img_name='serverperf/nodejs'
-container_name='server_perf_nodejs'
-shell_cmd='/bin/bash'
-start_cmd='/usr/bin/supervisord -c /docker/supervisord.conf'
-shared_volume_1="-v $PWD:/docker"
-port_map_1='5020:5020'
-port_map_2='5021:5021'
-port_map_3='4200:4200'
+container_name='server_perf_nodejs_native'
 
-networking_mapped="-p $port_map_1 -p $port_map_2 -p $port_map_3 \
--h $container_name.dkr"
-networking_native="--net host"
-networking=$networking_native
+start_cmd="$supervisord_cmd"
 
-volumes="$shared_volume_1"
+volumes="$shared_volume_base"
 
-docker stop $container_name && docker rm $container_name
+networking="$networking_native"
 
-docker_cmd="docker run -d -ti \
---name $container_name \
-$volumes \
-$networking \
-$img_name \
-$start_cmd"
-
-echo "running command... $docker_cmd"
-eval $docker_cmd
+docker_run
 
 #
-#native ports have to be allowed through the firewall individually, docker port-forwarding doesn't require this manual configuration, all ports exposed through docker containers are allow to pass through the firewall by default
+# native ports have to be allowed through the firewall individually,
+# docker port-forwarding doesn't require this manual configuration,
+# all ports exposed through docker containers are allow to pass
+# through the firewall by default
 #
-sudo firewall-cmd --add-forward-port=port=15020:proto=tcp:toport=5020
-sudo firewall-cmd --add-forward-port=port=15021:proto=tcp:toport=5021
-sudo firewall-cmd --add-forward-port=port=14200:proto=tcp:toport=4200
+# sudo firewall-cmd --add-forward-port=port=15020:proto=tcp:toport=5020
+# sudo firewall-cmd --add-forward-port=port=15021:proto=tcp:toport=5021
+# sudo firewall-cmd --add-forward-port=port=14200:proto=tcp:toport=4200
 
-sleep 3s
-docker ps -a
+#
+# open ports that services are running on in container
+#
+sudo firewall-cmd --add-port=5020/tcp
+sudo firewall-cmd --add-port=5021/tcp
+sudo firewall-cmd --add-port=4200/tcp
