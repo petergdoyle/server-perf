ea1628354efdbd79ab2568f1d1a7fa25b9f42e63
vagrant <vagrant@server-perf.vbx>
2016-01-06 06:00:05 -0500

    forget a file on last commit

--- nodejs/docker_build.sh
+++ nodejs/docker_build.sh
@@ -2,10 +2,11 @@
 
 . ../scripts/lib/docker_build.sh
 
-#if [ -d "node_modules" ]; then rm -vfr node_modules; fi
-#npm install
+if [ -d "node_modules" ]; then rm -vfr node_modules; fi
+npm install
 
+no_cache=$1
 
 img_name='server-perf/nodejs'
 
-docker_build $1
+docker_build $no_cache
