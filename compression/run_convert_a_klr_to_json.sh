5f35315fb926e7e634af5a880b8ed2416f2ef371
petergdoyle <peter.g.doyle@gmail.com>
2016-04-24 20:17:33 -0600

    fixed file path

--- compression/run_convert_a_klr_to_json.sh
+++ compression/run_convert_a_klr_to_json.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-./convert_klr_to_json.sh estreaming-data-json/Input-1V_J0B_ATLPAR-3412-0008-0924-172400-266.txt |sed '2d' |less
+fn='Input-1V_J0B_ATLPAR-3412-0008-0924-172400-266'
+
+./convert_klr_to_json.sh estreaming-data-json/$fn.txt > $fn.json
