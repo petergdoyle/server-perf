8333fe9280f403ee4a15c8b40bced8352ec40d04
vagrant <vagrant@server-perf.vbx>
2016-05-31 18:51:35 -0400

    fixed jetty download uri

--- jetty/clean_and_build.sh
+++ jetty/clean_and_build.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
+
+if [ ! -d base/webapps/ ]; then
+  mkdir -p base/webapps/ 
+fi
+
 rm -vfr base/webapps/* \
 && mvn --file ../java/server-perf-common/pom.xml clean install \
 && mvn --file ../java/server-perf-servlet/pom.xml clean install \
-&& cp -v ../java/servlet/target/servlet.war base/webapps/
+&& cp -v ../java/server-perf-servlet/target/servlet.war base/webapps/
